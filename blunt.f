b4b006e175cf8a2c72e006b9c6da219cff664697
xlopi001 <xlopi001>
2004-04-05 16:42:33 +0000

    modulo blunt_body added t

---
+++ blunt.f
@@ -0,0 +1,216 @@
+!----------------------------------------------------------------------
+      MODULE blunt_body
+!----------------------------------------------------------------------
+! ... This module compute the drag and lift forces on a solid block
+! ... by assuming that the main stream is flowing in the positive 
+! ... x-direction
+      USE dimensions
+      USE grid, ONLY: iob
+      USE parallel, ONLY: mpime, root
+      USE pressure_epsilon, ONLY: p
+      USE time_parameters, ONLY: time, dt
+      IMPLICIT NONE
+!      
+      INTEGER :: nblu(1:max_nblock) = 0
+      INTEGER :: ibl
+      REAL*8, ALLOCATABLE :: fdrag(:), flift(:)
+      REAL*8 :: pd
+
+      TYPE surface_pressure
+        INTEGER :: np
+        INTEGER :: ijk
+        INTEGER :: n(3)
+        REAL*8  :: ds
+        REAL*8  :: p
+      END TYPE surface_pressure
+      TYPE(surface_pressure), ALLOCATABLE :: surfp(:,:)
+      INTEGER :: perim
+!
+      SAVE
+!----------------------------------------------------------------------
+      CONTAINS
+!----------------------------------------------------------------------
+      SUBROUTINE set_blunt
+
+      USE dimensions
+      USE domain_decomposition, ONLY: cell_g2l, cell_owner
+      USE grid, ONLY: dx, dz, flag
+      INTEGER :: prm, l, m, n, i, k, ijk, imesh
+
+      perim = 0
+      prm   = 0
+      DO n = 1, no
+        IF (nblu(n) == 1) THEN
+          prm = 2*( iob(n)%zhi - iob(n)%zlo + iob(n)%xhi - iob(n)%xlo) + 4
+          perim = MAX( prm, perim )
+        END IF
+      END DO
+
+      IF (ANY( ABS(nblu(:)) > 1 )) CALL error('set_blunt','control blocks',1)
+      ALLOCATE(surfp(SUM(nblu), perim))
+
+      surfp(:,:)%p   = 0.D0
+      surfp(:,:)%ds  = 0.D0
+
+      m = 0
+      DO n = 1, no
+      IF (nblu(n) == 1) THEN
+        m = m + 1
+        l = 0
+        
+        i = iob(n)%xlo - 1
+        DO k = iob(n)%zlo, iob(n)%zhi
+          l = l+1
+          imesh = i + (k-1) * nx
+          IF (cell_owner(imesh) == mpime) THEN
+            ijk = cell_g2l(imesh,mpime)
+            IF (flag(ijk) == 1) THEN
+              surfp(m,l)%np   = mpime
+              surfp(m,l)%ijk  = ijk
+              surfp(m,l)%ds   = dz(k)
+            END IF
+          END IF
+          surfp(m,l)%n(1) = +1
+          surfp(m,l)%n(2) = 0
+          surfp(m,l)%n(3) = 0
+        END DO
+        k = iob(n)%zhi + 1
+        DO i = iob(n)%xlo, iob(n)%xhi
+          l = l+1
+          imesh = i + (k-1) * nx
+          IF (cell_owner(imesh) == mpime) THEN
+            ijk = cell_g2l(imesh,mpime)
+            IF (flag(ijk) == 1) THEN
+              surfp(m,l)%np   = mpime
+              surfp(m,l)%ijk  = ijk
+              surfp(m,l)%ds   = dx(i)
+            END IF
+          END IF
+          surfp(m,l)%n(1) = 0
+          surfp(m,l)%n(2) = 0
+          surfp(m,l)%n(3) = +1
+        END DO
+        i = iob(n)%xhi + 1
+        DO k = iob(n)%zhi, iob(n)%zlo, -1
+          l = l+1
+          imesh = i + (k-1) * nx
+          IF (cell_owner(imesh) == mpime) THEN
+            ijk = cell_g2l(imesh,mpime)
+            IF (flag(ijk) == 1) THEN
+              surfp(m,l)%np   = mpime
+              surfp(m,l)%ijk  = ijk
+              surfp(m,l)%ds   = dz(k)
+            END IF
+          END IF
+          surfp(m,l)%n(1) = -1
+          surfp(m,l)%n(2) = 0
+          surfp(m,l)%n(3) = 0
+        END DO
+        k = iob(n)%zlo - 1
+        DO i = iob(n)%xhi, iob(n)%xlo, -1
+          l = l+1
+          imesh = i + (k-1) * nx
+          IF (cell_owner(imesh) == mpime) THEN
+            ijk = cell_g2l(imesh,mpime)
+            IF (flag(ijk) == 1) THEN
+              surfp(m,l)%np   = mpime
+              surfp(m,l)%ijk  = ijk
+              surfp(m,l)%ds   = dx(i)
+            END IF
+          END IF
+          surfp(m,l)%n(1) = 0
+          surfp(m,l)%n(2) = 0
+          surfp(m,l)%n(3) = -1
+        END DO
+
+      END IF
+      END DO
+      
+      IF (m /= SUM(nblu)) CALL error('set_blunt','control nblu',m)
+        
+      END SUBROUTINE set_blunt
+!----------------------------------------------------------------------
+      SUBROUTINE bluntb
+      USE dimensions, ONLY: no
+!
+      IMPLICIT NONE
+      INTEGER :: n, m
+!
+! ... Integrate the gas pressure field around
+! ... the body to compute the drag and the lift 
+!
+      ALLOCATE(fdrag(SUM(nblu)))
+      ALLOCATE(flift(SUM(nblu)))
+
+      fdrag(:) = 0.D0
+      flift(:) = 0.D0
+
+      m = 0
+      DO n = 1, no
+        IF (nblu(n) == 1) THEN
+          m = m + 1
+
+          CALL dragbl(m, fdrag(m))
+          CALL liftbl(m, flift(m))
+
+          CALL parallel_sum_real(fdrag(m), 1)
+          CALL parallel_sum_real(flift(m), 1)
+
+        END IF
+      END DO
+
+      IF (m /= SUM(nblu)) CALL error('set_blunt','control nblu',m)
+        
+!
+! ... Write out the forces acting on the body
+      IF (mpime == root) CALL print_action
+!
+      DEALLOCATE(fdrag, flift)
+!
+      RETURN
+      END SUBROUTINE bluntb
+!----------------------------------------------------------------------
+      SUBROUTINE dragbl(m,fd)
+      IMPLICIT NONE
+!
+        INTEGER, INTENT(IN) :: m
+	REAL*8, INTENT(OUT) :: fd
+        INTEGER :: pp, ijk
+!
+        fd = 0.D0
+        DO pp = 1, perim
+          ijk = surfp(m,pp)%ijk
+          IF (mpime == surfp(m,pp)%np) surfp(m,pp)%p = p(ijk)
+          fd = fd + surfp(m,pp)%ds * surfp(m,pp)%p * surfp(m,pp)%n(1)
+	END DO
+!
+      END SUBROUTINE dragbl
+!----------------------------------------------------------------------
+      SUBROUTINE liftbl(m,fl)
+      IMPLICIT NONE
+!
+        INTEGER, INTENT(IN) :: m
+	REAL*8, INTENT(OUT) :: fl
+        INTEGER :: pp, ijk
+!
+        fl = 0.D0
+        DO pp = 1, perim
+          ijk = surfp(m,pp)%ijk
+          IF (mpime == surfp(m,pp)%np) surfp(m,pp)%p = p(ijk)
+          fl = fl + surfp(m,pp)%ds * surfp(m,pp)%p * surfp(m,pp)%n(3)
+	END DO
+!
+      END SUBROUTINE liftbl
+!----------------------------------------------------------------------
+      SUBROUTINE print_action
+      USE time_parameters, ONLY: time
+      IMPLICIT NONE
+
+      WRITE(15,200) time, fdrag(:), flift(:)
+
+ 200  FORMAT(F14.6,20(G18.6))
+
+      END SUBROUTINE print_action
+!----------------------------------------------------------------------
+      END MODULE blunt_body
+!----------------------------------------------------------------------
