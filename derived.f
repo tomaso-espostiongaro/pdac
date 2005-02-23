96f4bc24b891ca1e233dead1f259fb4d708d472f
xlopi001 <xlopi001>
2005-02-23 17:47:25 +0000

    bug fixed t

--- derived.f
+++ derived.f
@@ -46,7 +46,7 @@
       INTEGER, INTENT(IN) :: expmin
       REAL, DIMENSION(SIZE(array)) :: clamp
 
-      clamp = MAX(10.E0**expmin, array)
+      clamp = MAX(1.E0**expmin, array)
 
       RETURN
       END FUNCTION clamp
